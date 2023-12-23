09fc12cd9f60df8de82669dc41266e44a9282ab9
AntoineRiouxDemers <152810044+AntoineRiouxDemers@users.noreply.github.com>
2023-12-22 20:42:12 -0500

    Add DimensionBtn prompt for grid dimension

--- javascript.js
+++ javascript.js
@@ -1,1 +1,18 @@
-const gridDiv = document.getElementById(grid);
+const gridDiv = document.getElementById('grid');
+const dimensionBtn = document.getElementById('dimensionBtn');
+
+var dimensionGrid = 16;
+
+dimensionBtn.addEventListener('click', () => dimensionClicked());
+
+function dimensionClicked(){
+    var tempDimensionGrid = parseInt(prompt('Dimension grid needed (MAX 100):'));
+
+    if(isNaN(tempDimensionGrid) ||
+     tempDimensionGrid < 2 ||
+     tempDimensionGrid > 100){
+        alert('A number between 2 and 100 must be entered!');
+     } else{
+        dimensionGrid = tempDimensionGrid;
+     }
+}
