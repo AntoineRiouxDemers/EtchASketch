aef25324aa84c03e05e4160ef534af4b2d512a76
AntoineRiouxDemers <152810044+AntoineRiouxDemers@users.noreply.github.com>
2023-12-22 23:18:54 -0500

    Add basic grid creation and modification

--- javascript.js
+++ javascript.js
@@ -1,9 +1,25 @@
 const gridDiv = document.getElementById('grid');
 const dimensionBtn = document.getElementById('dimensionBtn');
-
-var dimensionGrid = 16;
-
+var currentDimension;
 dimensionBtn.addEventListener('click', () => dimensionClicked());
+
+if(currentDimension == null) setGrid(16);
+
+function setGrid(dimension){
+    gridDiv.innerHTML = '';
+    currentDimension = dimension;
+
+    for(let i = 0; i < dimension; i++){
+        let column = gridDiv.appendChild(document.createElement('div'));
+        column.classList.add('columns');
+        column.setAttribute('style', 'Display: flex; justify-content: center; align-items: center;');
+        for(let i = 0; i< dimension; i++){
+            let pixel = column.appendChild(document.createElement('div'));
+            pixel.classList.add('pixels')
+            pixel.setAttribute('style', 'border: solid 0.1px; height: 1px; width: 1px;');
+        }
+    }
+}
 
 function dimensionClicked(){
     var tempDimensionGrid = parseInt(prompt('Dimension grid needed (MAX 100):'));
@@ -13,6 +29,6 @@ function dimensionClicked(){
      tempDimensionGrid > 100){
         alert('A number between 2 and 100 must be entered!');
      } else{
-        dimensionGrid = tempDimensionGrid;
+        setGrid(tempDimensionGrid);
      }
 }
