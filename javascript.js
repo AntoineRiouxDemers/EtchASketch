8341079a044b0959fd10898dff9f7d2cba8d34f5
AntoineRiouxDemers <152810044+AntoineRiouxDemers@users.noreply.github.com>
2023-12-24 14:32:09 -0500

    Add rgb to working buttons
    Had to fix the way the pixels change background color to make everything
    work properly

--- javascript.js
+++ javascript.js
@@ -6,35 +6,59 @@ const progressiveBtn = document.getElementById('progressBtn');
 const blackBtn = document.getElementById('blackBtn');
 const gridDimension = 30 //dimension in vertical width VW set for the grid itself
 var currentDimension;
+var currentColor = 'black';
 
 dimensionBtn.addEventListener('click', () => dimensionClicked());
 if(currentDimension == null) setGrid(16);
 
 
-
+//main game function
 function game(){
 
     let pixels = document.querySelectorAll('.pixels');
 
     pixels.forEach(pixel => {
         pixel.addEventListener('mouseover', function () {
-            pixel.classList.add('hover');
+            setColor(pixel);
+            console.log(pixel.classList);
+
         });
     });
 
     clearBtn.addEventListener('click', () => clearGrid());
-    // rgbBtn.addEventListener('click', () => setHover('rgb'));
-    // blackNwhiteBtn.addEventListener('click', () => setHover('black'))
+    rgbBtn.addEventListener('click', () => currentColor = 'rgb');
+    blackBtn.addEventListener('click', () => currentColor = 'black');
 }
 
+//Set Color of pixel when mouse hover it
+function setColor(pixel, isAlreadyColored){
+    switch(currentColor){
+        case 'rgb':
+            console.log('rgb(' + getRandomColor() + ')')
+            pixel.style.backgroundColor = 'rgb(' + getRandomColor() + ')';
+            break;
+        case 'progressive':
+            break;
+        default:
+            pixel.style.backgroundColor = currentColor;
+    }
+}
+
+//Return a random rgb array
+function getRandomColor(){
+    return [Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)];
+}
+
+//Clearing the Grid
 function clearGrid(){
     let pixels = document.querySelectorAll('.pixels');
 
     pixels.forEach(pixel => {
-        pixel.classList.remove('hover');
+        pixel.style.backgroundColor = '';
     }); 
 }
 
+//Setting up the Grid with dimension by default or asked
 function setGrid(dimension){
     gridDiv.innerHTML = '';
     currentDimension = dimension;
@@ -45,13 +69,14 @@ function setGrid(dimension){
         column.setAttribute('style', 'Display: flex; width: ' + gridDimension + 'vw; height: ' + gridDimension / dimension + 'vw;');
         for(let i = 0; i< dimension; i++){
             let pixel = column.appendChild(document.createElement('div'));
-            pixel.classList.add('pixels')
+            pixel.classList.add('pixels');
             pixel.setAttribute('style', 'height: ' + gridDimension / dimension + 'vw; width: ' + gridDimension / dimension + 'vw;');
         }
     }
     game();
 }
 
+//Prompt when dimension button is clicked to setup the Grid
 function dimensionClicked(){
     var tempDimensionGrid = parseInt(prompt('Dimension grid needed (MAX 100):'));
 
